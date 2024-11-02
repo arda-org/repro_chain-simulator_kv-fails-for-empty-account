bde3f12353ddd5971a78f041599d1a97e507bc94
Lucas Willems <lcswillems@gmail.com>
2024-11-02 14:16:22 +0100

    Add succeeding test

--- contract.test.ts
+++ contract.test.ts
@@ -1,9 +1,16 @@
 import { expect, test } from "vitest";
 import { FSWorld } from "xsuite";
 
-test("Test", async () => {
+test("Fails", async () => {
   using world = await FSWorld.start({ saveLogs: true });
   const wallet = await world.createWallet({ balance: 1 });
   expect(await wallet.getAccountBalance()).toEqual(1n);
-  await wallet.getAccountValue("01");
+  expect(await wallet.getAccountValue("01")).toEqual("");
+});
+
+test("Succeeds", async () => {
+  using world = await FSWorld.start({ saveLogs: true });
+  const wallet = await world.createWallet({ balance: 1, kvs: { "": "" } });
+  expect(await wallet.getAccountBalance()).toEqual(1n);
+  expect(await wallet.getAccountValue("01")).toEqual("");
 });
