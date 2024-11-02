471f64b87cede7c588086c535af4008e47af15a0
Lucas Willems <lcswillems@gmail.com>
2024-11-02 14:11:50 +0100

    Set balance

--- contract.test.ts
+++ contract.test.ts
@@ -1,8 +1,9 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
 import { FSWorld } from "xsuite";
 
 test("Test", async () => {
   using world = await FSWorld.start({ saveLogs: true });
-  const wallet = await world.createWallet();
+  const wallet = await world.createWallet({ balance: 1 });
+  expect(await wallet.getAccountBalance()).toEqual(1n);
   await wallet.getAccountValue("01");
 });
